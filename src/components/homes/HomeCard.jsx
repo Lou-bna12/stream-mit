82c61b190f1fedbf8bbbe6c12e161cc3b711a292
Loubna Sellam <loubnasellam49@gmail.com>
2023-08-10 13:28:36 +0200

    git commit

--- src/components/homes/HomeCard.jsx
+++ src/components/homes/HomeCard.jsx
@@ -1,48 +1,62 @@
-import React from 'react'
+import React from "react"
+import { Link } from "react-router-dom"
 
-export const HomeCard = ({item:{id, cover, name, rating, time, desc, starring, genres, tags, video } } ) => {
-  return(
+const HomeCard = ({ item: { id, cover, name, rating, time, desc, starring, genres, tags, video } }) => {
+  return (
     <>
-
-    <div className='box'>
-      <div className='coverImage'>
-        <img src={cover} alt='' />
-      </div>
-
-      <div className="content flex">
-        <div className='details row '>
-        <h1>{name}</h1>
-        <div className="rating flex">
-          <i className='fa fa-start'></i>
-          <i className='fa fa-start'></i>
-          <i className='fa fa-start'></i>
-          <i className='fa fa-start'></i>
-          <i className='fa fa-start-half'></i>
+      <div className='box'>
+        <div className='coverImage'>
+          <img src={cover} alt='' />
         </div>
-        <label>{rating}</label>
-        <span>Classement</span>
-        <label>{time}</label>
+        <div className='content flex'>
+          <div className='details row'>
+            <h1>{name}</h1>
+            <div className='rating flex'>
+              <div className='rate'>
+                <i className='fas fa-star'></i>
+                <i className='fa fa-star'></i>
+                <i className='fa fa-star'></i>
+                <i className='fa fa-star'></i>
+                <i className='fa fa-star-half'></i>
+              </div>
+              <label>{rating}(Imdb)</label>
+              <span>Classement</span>
+              <label>{time}</label>
+            </div>
+            <p>{desc}</p>
+            <div className='cast'>
+              <h4>
+                <span>Vedettes </span>
+                {starring}
+              </h4>
+              <h4>
+                <span>Genres </span>
+                {genres}
+              </h4>
+              <h4>
+                <span>Tags </span>
+                {tags}
+              </h4>
+            </div>
+            <button className='primary-btn'>
+              <i className='fas fa-play'></i> JOUEZ MAINTENANT 
+            </button>
+          </div>
+          <div className='palyButton row'>
+            <Link to={`/singlepage/${id}`}>
+              <button>
+                <div className='img'>
+                  <img src='./images/play-button.png' alt='' />
+                  <img src='./images/play.png' alt="" className='change' />
+                </div>
+                VOIR LA BANDE-ANNONCE
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
-      </div>
-      <p>{desc}</p>
-      <div className="cast">
-      <h4>
-        <span>Vedettes</span>
-        {starring}
-        </h4> 
-        <h4>
-        <span>Genres</span>
-        {genres}
-        </h4> 
-        <h4>
-        <span>Tags</span>
-        {tags}
-        </h4> 
-      </div>
-      <button className='primary-btn'>
-        <i className='fas fa-play'></i>Jouez maintenant
-      </button>
-    </div>
     </>
   )
 }
+
+export default HomeCard
