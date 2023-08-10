c5839fbd312186af28dbf050a9b48e5a2b3da11f
Loubna Sellam <loubnasellam49@gmail.com>
2023-08-10 17:57:04 +0200

    git commit

--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
 import Homes from '../components/homes/Homes'
 import { Upcomming } from '../components/Upcomming/Upcomming'
-import { latest, upcome } from '../dummyData'
+import { latest, recommended, upcome } from '../dummyData'
 import Trending from '../components/trending/Trending'
 
 export const HomePage = () => {
       const [items, setitems] = useState(upcome)
       const [item, setitem] = useState(latest)
+      const [rec, setrec] = useState(recommended)
   return (
    <>
   <Homes />
   <Upcomming items={items} title='Films à Venir' />
-  <Upcomming items={item} title='Les plus récents' />
+  <Upcomming items={item} title='Les plus Récents' />
   <Trending  />
+  <Upcomming items={rec} title='Films Recommandés' />
   </>
 )  
 }
